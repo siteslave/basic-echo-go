655239e54b3042c0bbd3160c1ae009c4e1ef115f
Satit Rianpit <siteslave@msn.com>
2021-02-04 23:14:00 +0700

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gitlab.com/siteslave/demo-go
+
+go 1.15
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/jinzhu/gorm v1.9.16
+	github.com/labstack/echo/v4 v4.1.17
+)
---
+++ server.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type User struct {
+	UserId uint `gorm:"primary_key" json: "user_id"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	Email string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Database struct {
+	DB *gorm.DB
+}
+
+type jwtCustomClaims struct {
+	Name  string `json:"name"`
+	Admin bool   `json:"admin"`
+	jwt.StandardClaims
+}
+
+func main() {
+	e := echo.New()
+
+	  // Middleware
+  e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, world!")
+	})
+
+	d := Database{}
+	d.Initialize()
+
+
+	e.POST("/login", d.Login)
+
+
+	r := e.Group("/api")
+
+	config := middleware.JWTConfig{
+		Claims:     &jwtCustomClaims{},
+		SigningKey: []byte("secret"),
+	}
+
+	r.Use(middleware.JWTWithConfig(config))
+
+	r.GET("/users/:id", d.GetInfo)
+	r.GET("/users", d.GetAll)
+	r.POST("/users", d.Create)
+
+	r.PUT("/users/:id", d.Update)
+	r.DELETE("/users/:id", d.Delete)
+
+	r.GET("/test-jwt", TestJwt)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func (d *Database) Initialize() {
+	db, err := gorm.Open("mysql", "root:789124@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=True");
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d.DB = db
+}
+
+func (d *Database) GetAll(c echo.Context) error {
+	users := []User{}
+	if err := d.DB.Find(&users).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	return c.JSON(http.StatusOK, users)
+}
+
+func (d *Database) GetInfo(c echo.Context) error {
+	id := c.Param("id")
+
+	user := User{}
+	if err := d.DB.First(&user, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	return c.JSON(http.StatusOK, user)
+}
+
+func (d *Database) Create(c echo.Context) error {
+
+	firstName := c.FormValue("firstName")
+	lastName := c.FormValue("lastName")
+	email := c.FormValue("email")
+
+	result := d.DB.Create(&User{FirstName: firstName, LastName: lastName, Email: email})
+	
+	if result.Error != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, result.Value)
+}
+
+func (d *Database) Update(c echo.Context) error {
+
+	id := c.Param("id")
+	user := User{}
+
+	firstName := c.FormValue("firstName")
+	lastName := c.FormValue("lastName")
+	email := c.FormValue("email")
+
+	if  err := d.DB.Find(&user, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	user.FirstName = firstName
+	user.LastName = lastName
+	user.Email = email 
+
+	if err := d.DB.Save(&user).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, user)
+}
+
+func (d *Database) Delete(c echo.Context) error {
+	id := c.Param("id")
+
+	if err := d.DB.Delete(&User{}, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
+
+func TestJwt(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*jwtCustomClaims)
+	name := claims.Name
+
+	return c.String(http.StatusOK, "Welcome "+name+"!")
+}
+
+func (d *Database) Login(c echo.Context) error {
+	user := User{}
+
+	username := c.FormValue("username")
+	password :=c.FormValue("password")
+
+	result := d.DB.Where(&User{Username: username, Password: password}).Find(&user)
+
+	if result.Error != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	// Create token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = "Satit Rianpit"
+	claims["admin"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"token": t,
+	})
+
+}
